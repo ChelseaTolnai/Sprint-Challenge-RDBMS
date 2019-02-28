1412921b7909053431111ef087220d2ee862ae67
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-02-28 14:04:04 -0500

    Implemented actions endpoints to get, update and delete entries.

--- api/Actions/actionsRouter.js
+++ api/Actions/actionsRouter.js
@@ -13,12 +13,62 @@ actionsRouter.post('/', actionCheck, async (req, res) => {
     }
 });
 
+actionsRouter.get('/', async (req, res, next) => {
+    try {
+        const actions = await db.get();
+        res.status(200).json(actions);
+    } catch (err) {
+        next({code: 500, action: 'getting', subject: 'actions'});
+    }
+});
+
+actionsRouter.get('/:id', actionIdExists, async (req, res, next) => {
+    try {
+        const action = await db.get(req.params.id);
+        res.status(200).json(action);
+    } catch (err) {
+        next({code: 500, action: 'getting', subject: 'action'});
+    }
+});
+
+actionsRouter.delete('/:id', actionIdExists, async (req, res, next) => {
+    try {
+        const action = await db.get(req.params.id);
+        await db.remove(req.params.id);
+        res.status(200).json({...action, deleted: 'successful'});
+    } catch (err) {
+        next({code: 500, action: 'deleting', subject: 'action'});
+    }
+});
+
+actionsRouter.put('/:id', actionIdExists, actionCheck, async (req, res, next) => {
+    try {
+        await db.update(req.params.id, {...req.body});
+        const updatedAction = await db.get(req.params.id);
+        res.status(200).json({...updatedAction, updated: 'successful'});
+
+    } catch (error) {
+        next({code: 500, action: 'updating', subject: 'action'});
+    }
+});
+
 function actionCheck (req, res, next) {
     if (!req.body.project_id || !req.body.description || !req.body.notes) {
         next({code: 400, action: 'updating', subject: 'action. Project ID, description and notes required'})
         return;
     } else {
         next();
+    }
+};
+
+async function actionIdExists (req, res, next) {
+    try {
+        const action = await db.get(req.params.id);
+        if (action) {
+            next();
+        }
+    } catch (err) {
+        next({code: 404, action: 'finding', subject: 'action. Action with specified ID does not exist'});
     }
 };
 
--- data/helpers/actionsModel.js
+++ data/helpers/actionsModel.js
@@ -6,4 +6,27 @@ module.exports = {
     insert: function(action) {
         return query.insert(action)
     },
+    get: function(id) {
+        if (id) {
+            return db('actions')
+            .where({ id })
+            .first()
+            .then(action => this.mapComplete(action));
+        }
+
+        return db('actions').select('*').map(action => this.mapComplete(action));
+    },
+    remove: function(id) {
+        return db('actions').where({ id }).del();
+    },
+    update: function(id, changes) {
+        return db('actions')
+        .where({ id })
+        .update(changes)
+        .then(count => (count > 0 ? this.get(id) : null));
+    },
+    mapComplete: function(object) {
+        if (object.completed === 0) {return {...object, completed: false} }
+        else {return {...object, completed: true} }
+    },
 };
